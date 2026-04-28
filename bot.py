d2d3f414997956090724a67d97613d3524cad42c
chupikovnikodim <chupikovnikodim@gmail.com>
2026-04-29 00:59:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,16 +3,18 @@ import os
 
 TOKEN = os.getenv("TG_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-API_KEY = os.getenv("OPENAI_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
-url = f"https://newsapi.org/v2/everything?q=IT OR AI OR backend OR fullstack&language=en&sortBy=publishedAt&pageSize=5&apiKey={API_KEY}"
+url = f"https://newsapi.org/v2/everything?q=IT OR AI OR backend&language=en&sortBy=publishedAt&pageSize=5&apiKey={NEWS_API_KEY}"
 
 news = requests.get(url).json()
-
 articles = news.get("articles", [])
 
-message = "📰 IT новини:\n\n"
+if not articles:
+    print("Немає новин або помилка API:", news)  # для дебагу в Actions logs
+    exit()
 
+message = "📰 IT новини:\n\n"
 for a in articles:
     message += f"{a['title']}\n{a['url']}\n\n"
 
